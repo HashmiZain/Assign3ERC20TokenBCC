32b3d9c1dec33e0019ba28b209fd34ec6c4cb595
HashmiZain <72328649+HashmiZain@users.noreply.github.com>
2021-09-26 12:21:42 +0500

    Update eth_transaction.js

--- Assignment_web3/eth_transaction.js
+++ Assignment_web3/eth_transaction.js
@@ -5,8 +5,8 @@ const web3 = new Web3('https://ropsten.infura.io/v3/9b02a81a69074206bd1e97a7b834
 const account1 = '0x9b64Ec623E24d67dB5C5a38AE93BE98a873bD731' // account address 1
 const account2 = '0x25b3C00CE5030ABDBCb4B521F5ac67B4176E1097' // account address 2
 
-const privateKey1 = Buffer.from('7c05a6d12d3e77f1d8b4463a5ca16d23991dc3946868d5ef9060181212af8e0f', 'hex')
-const privateKey2 = Buffer.from('2d777080cb6a92f3bc6f2c632c726e79f36c807b5a8a9b07b9799ff65393b66a', 'hex')
+const privateKey1 = Buffer.from('private key 1', 'hex')
+const privateKey2 = Buffer.from('private key 2', 'hex')
 
 
 web3.eth.getTransactionCount(account1, (err, txCount) => {
